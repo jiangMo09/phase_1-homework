262e8d8922bd0e45e07cf4be85aea00a6c2841d6
jiangMo09 <mmiao0641@gmail.com>
2024-05-06 16:44:35 +0800

    [FEAT]week6 - task1 - 連接 mysql 與 name 丟給 member

--- week6/main.py
+++ week6/main.py
@@ -4,6 +4,20 @@ from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from starlette.middleware.sessions import SessionMiddleware
+import mysql.connector
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+def get_db_connection():
+    connection = mysql.connector.connect(
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
+    )
+    return connection
 
 secret_key = secrets.token_bytes(32)
 
@@ -13,26 +27,54 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 
+def connection_mysql():
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    print("建立連線")
+    return connection, cursor
+
+def close_mysql(cursor, connection):
+    cursor.close()
+    connection.close()
+    print("關閉連線")
+
 @app.get("/", response_class=HTMLResponse)
 def index(request: Request):
     return templates.TemplateResponse(
-        "index.html", {"request": request, "header_text": "歡迎光臨，請輸入帳號密碼"}
+        "index.html", {"request": request, "header_text": "歡迎光臨，請註冊登入系統"}
     )
 
 
 @app.post("/signin")
 def signin(request: Request, username: str = Form(None), password: str = Form(None)):
     error_message = None
+    name = None
 
     if not username or not password:
         error_message = "Please enter username and password"
-    elif username != "test" or password != "test":
-        error_message = "Username or password is not correct"
+    else:
+        connection, cursor = connection_mysql()
+
+        try:
+            query = "SELECT * FROM member WHERE username = %s AND password = %s"
+            values = (username, password)
+            cursor.execute(query, values)
+            user = cursor.fetchone()
+
+            if user:
+                name = user[1]
+            else:
+                error_message = "帳號或是密碼輸入錯誤"
+        except mysql.connector.Error as err:
+            error_message = f"Error connecting to MySQL: {err}"
+        finally:
+            close_mysql(cursor, connection)
 
     if error_message:
         return RedirectResponse(f"/error?message={error_message}", status_code=302)
 
     request.session["SIGNED_IN"] = True
+    request.session["NAME"] = name 
     return RedirectResponse("/member", status_code=302)
 
 
@@ -47,9 +89,10 @@ def member(request: Request):
     if not request.session.get("SIGNED_IN", False):
         return RedirectResponse("/", status_code=302)
 
+    name = request.session.get("NAME", "")
     response = templates.TemplateResponse(
         "/member/index.html",
-        {"request": request, "header_text": "歡迎光臨，這是會員頁"},
+        {"request": request, "header_text": "歡迎光臨，這是會員頁", "name": name},
     )
     response.headers["Cache-Control"] = "no-cache, no-store"
 
